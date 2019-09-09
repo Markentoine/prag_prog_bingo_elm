2e4c97fbbbbe6a1ecdf55b9245fb989e614bf0df
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-09-09 14:54:10 +0200

    update.

--- Bingo.elm
+++ Bingo.elm
@@ -2,32 +2,67 @@ module Bingo exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
-playerInfo : String -> Int -> String
-playerInfo name gameNumber =
-    name ++ " - Game #" ++ toString gameNumber
+
+-- UPDATE
+
+
+type Msg
+    = NewGame
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NewGame ->
+            { model | gameNumber = model.gameNumber + 1 }
 
 
 
 -- MODEL
 
 
-initalModel =
-    { name = "Jerome"
-    , gameNumber = 1
-    , entries = intialEntries
+type alias Model =
+    { name : String
+    , gameNumber : Int
+    , entries : List Entry
     }
 
 
+type alias Entry =
+    { id : Int
+    , phrase : String
+    , points : Int
+    , marked : Bool
+    }
+
+
+initialModel : { name : String, gameNumber : Int, entries : List Entry }
+initialModel =
+    { name = "Jerome"
+    , gameNumber = 1
+    , entries = initialEntries
+    }
+
+
+initialEntries : List Entry
 initialEntries =
-    [ { id = 1, phrase = "Future-proof", points = 100, marked = False }
-    , { id = 1, phrase = "Doing Agile", points = 200, marked = False }
+    [ Entry 1 "Future-proof" 100 False
+    , Entry 2 "Doing Agile" 200 False
+    , Entry 3 "In The Cloud" 300 False
+    , Entry 4 "Rock-Star Ninja" 400 False
     ]
 
 
 
 -- VIEW
+
+
+playerInfo : String -> Int -> String
+playerInfo name gameNumber =
+    name ++ " - Game #" ++ toString gameNumber
 
 
 viewHeader : String -> Html msg
@@ -48,6 +83,21 @@ viewPlayer name gameNumber =
         [ playerInfoText ]
 
 
+viewEntryItem : Entry -> Html msg
+viewEntryItem entry =
+    li []
+        [ span [ class "phrase" ] [ text entry.phrase ]
+        , span [ class "points" ] [ text (toString entry.points) ]
+        ]
+
+
+viewEntryList : List Entry -> Html msg
+viewEntryList entries =
+    entries
+        |> List.map viewEntryItem
+        |> ul []
+
+
 viewFooter : Html msg
 viewFooter =
     footer []
@@ -56,15 +106,18 @@ viewFooter =
         ]
 
 
-view : Html msg
-view =
+view : Model -> Html msg
+view model =
     div [ class "content" ]
         [ viewHeader "BUZZWORD BINGO"
-        , viewPlayer "jerome" 9
+        , viewPlayer model.name model.gameNumber
+        , viewEntryList model.entries
+        , div [ class "button-group" ]
+            [ button [ onClick NewGame ] [ text "New Game" ] ]
         , viewFooter
         ]
 
 
 main : Html msg
 main =
-    view
+    view initialModel
